179fca66893bd3945589490b5ea2ee61875ca4d5
KEITA Arai <arai@unio.co.jp>
2010-06-15 16:05:27 +0900

    sinatraを分離して、WSをまとめた

---
+++ web.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'cgi'
+require 'json'
+require 'sinatra'
+#require 'sinatra/base'
+require 'thin'
+require 'logger'
+# coding: utf8
+
+get '/' do
+  erb :index
+end
+
+post '/room' do
+  @room = CGI.escape(params[:title])
+  @user = CGI.escape(params[:user])
+  erb :room
+end
+
+get '/room' do
+  redirect '/'
+end
